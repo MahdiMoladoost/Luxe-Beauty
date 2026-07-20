80de1ad2b71be3975e9843640129e21fbeabd0b5
MahdiMoladoost <142019797+MahdiMoladoost@users.noreply.github.com>
2026-07-20 23:26:53 +0330

    Assemble embedded hero image

---
+++ components/home/hero-image.ts
@@ -0,0 +1,8 @@
+import part1 from "./hero-image-part-1"
+import part2 from "./hero-image-part-2"
+import part3 from "./hero-image-part-3"
+import part4 from "./hero-image-part-4"
+import part5 from "./hero-image-part-5"
+import part6 from "./hero-image-part-6"
+
+export const heroImage = `data:image/webp;base64,${part1}${part2}${part3}${part4}${part5}${part6}`
